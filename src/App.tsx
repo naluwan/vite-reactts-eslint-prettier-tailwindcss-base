6c6b3d7037511172d251c9311c0a51bc3185ad40
naluwan <street.baller06@gmail.com>
2023-09-07 10:59:39 +0800

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='mx-auto w-full text-center text-xl'>
+      Vite + React + Typescript + Eslint + Prettier + TailWindCSS
+    </div>
+  );
+};
+
+export default App;
